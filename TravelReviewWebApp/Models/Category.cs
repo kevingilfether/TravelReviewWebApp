c694e44f20d4632c3ca15f2e0845bb29f2fcde73
kevingilfether <kevingilfether@gmail.com>
2017-10-27 12:03:42 -0400

    Added code for to set up models, haven't scaffolded

---
+++ TravelReviewWebApp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewWebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Display(Name="Category")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelReviewWebApp/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewWebApp.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [Display(Name ="Date Published")]
+        public DateTime PublishDate { get; set; }
+        [MinLength(1), MaxLength(5)]
+        public int Stars { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
